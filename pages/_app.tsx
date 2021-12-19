a135dbfcfd9caad8642dfa8b42e475b0809a0519
Haiko Nguyen <Hai.NGUYEN.ext@sodexo.com>
2021-12-19 12:03:25 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import { TinaEditProvider } from 'tinacms/dist/edit-state'
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
 import '../styles/index.css'
+import {AppProps} from "next/app";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <TinaEditProvider
+        editMode={
+          <TinaCMS
+            clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
+            branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
+            isLocalClient={Boolean(
+              Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT ?? true)
+            )}
+            {...pageProps}
+          >
+            {(livePageProps: JSX.Element) => <Component {...livePageProps} />}
+          </TinaCMS>
+        }
+      >
+        <Component {...pageProps} />
+      </TinaEditProvider>
+    </>
+  )
 }
+
+export default App
