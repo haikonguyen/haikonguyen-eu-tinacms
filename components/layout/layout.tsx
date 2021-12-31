df68fe882fc9ef0ea67c7387f5d4630eba00d351
Haiko Nguyen <Hai.NGUYEN.ext@sodexo.com>
2021-12-31 17:45:00 +0100

    properly render NavBar based on the ResponsiveAppBar

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,18 +1,16 @@
-import Footer from './footer';
-import Meta from './meta';
-import { ReactNode } from 'react';
+import Footer from '../footer';
+import Meta from '../meta';
 import { Global } from '@emotion/react';
 import useGlobalStyle from '@styles/global';
+import { LayoutProps } from '@components/layout/types';
+import NavBar from '@components/navbar/navbar';
 
-type Props = {
-  children: ReactNode;
-};
-
-const Layout = ({ children }: Props) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Meta />
       <Global styles={useGlobalStyle} />
+      <NavBar />
       <main>{children}</main>
       <Footer />
     </>
